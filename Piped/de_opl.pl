781479670bfe2123dc53d80cf657250ed8fe6089
Wes <wes_peacock@sil.org>
2021-02-26 15:38:48 -0700

    Give better usage comments to show STDIN STDOUT usage

--- Piped/de_opl.pl
+++ Piped/de_opl.pl
@@ -1,4 +1,6 @@
-# perl -CS -pf de_opl.pl FileNameAfteropl.SFM
+# perl -CS -pf opl.pl FileName.OPL >FileName.SFM
+#or
+# perl -CS -pf opl.pl <FileName.OPL >FileName.SFM
 # undoes opl.pl & opl-bsl
 # Converts SFM file from one record per line to one field per line
 # occurences of '_hash_'  are changed to back to '#'
